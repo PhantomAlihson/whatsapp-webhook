a6737a5b6f4dca27776b6f9708c504e8a8319627
PhantomAlihson <alihson43@hotmail.com>
2026-03-05 11:58:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,28 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.get("/")
+VERIFY_TOKEN = "meutoken123"
+
+@app.route("/", methods=["GET"])
 def home():
     return "Servidor Railway funcionando!"
 
-@app.post("/webhook")
+@app.route("/webhook", methods=["GET", "POST"])
 def webhook():
-    data = request.get_json(silent=True)
-    print("Webhook recebido:", data)
-    return "EVENT_RECEIVED", 200
+
+    # Verificação do webhook da Meta
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Verification failed", 403
+
+    # Recebendo eventos (mensagens)
+    if request.method == "POST":
+        data = request.get_json()
+        print("Webhook recebido:", data)
+        return "EVENT_RECEIVED", 200
